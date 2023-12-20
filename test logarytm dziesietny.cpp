686b774014b359c19345e32c572527c183b30e0b
NataliaBronska <147701467+NataliaBronska@users.noreply.github.com>
2023-12-20 21:58:05 +0100

    test logarytm dziesiętny

---
+++ test logarytm dziesietny.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include"../logarytm dziesiętny/funkcja.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testlogarytmdziesietny
+{
+	TEST_CLASS(testlogarytmdziesietny)
+	{
+	public:
+
+		TEST_METHOD(liczba_ujemna)
+		{
+
+		
+			Assert::IsTrue(std::isnan(logarytm_dziesietny(-5.0)));
+		}
+
+		TEST_METHOD(jedynka)
+		{
+			Assert::AreEqual(0.0, logarytm_dziesietny(1.0));
+		}
+
+		TEST_METHOD(duza_liczba)
+		{
+			Assert::AreEqual(6.0, logarytm_dziesietny(1.0e6));
+		}
+	};
+}
