0bc36914d9030148b61795e15336141e46b3705e
NataliaBronska <147701467+NataliaBronska@users.noreply.github.com>
2023-12-20 22:02:58 +0100

    funkcje.cpp - kalkulatoe

---
+++ funkcje.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+
+double logarytm_naturalny(double liczba)
+{
+	return log(liczba);
+}
+double odchylenie_standardowe(double x, double y)
+{
+	return sqrt((x * x + y * y) / 2.0);
+}
+double exponent(double liczba)
+{
+	return exp(liczba);
+}
