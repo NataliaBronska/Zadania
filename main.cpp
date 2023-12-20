1940665a65f0bf6bb65ea585d12becb7a74bc1c9
NataliaBronska <147701467+NataliaBronska@users.noreply.github.com>
2023-12-20 22:03:25 +0100

    main.cpp -kalkulator

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include "funkcja.h"
-
+#include "funkcje.h"
 using namespace std;
-
 int main()
 {
-	double liczba;
-	cout << "Podaj liczbe: " << endl;
-	cin >> liczba;
-	cout << "Logarytm dziesietny z " << liczba << " to: " << logarytm_dziesietny(liczba) << endl;
+	double x, y;
+	cout << "Podaj 2 liczby: ";
+	cin >> x >> y;
+	cout << "Logarytm naturalny z pierwszej liczby to: " << logarytm_naturalny(x) << endl;
+	cout << "Odchylenie standardowe z 2 podanych liczb wynosi: " << odchylenie_standardowe(x, y) << endl;
+	cout << "Wartosc exponentu z pierwszej liczby to: " << exponent(x) << endl;
+	return(0);
 
-	return (0);
 }
